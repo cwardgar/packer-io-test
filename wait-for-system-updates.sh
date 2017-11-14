104085753782a5e9b80cd04a3cceaff584c01986
cwardgar <cwardgar@gmail.com>
2017-11-13 23:44:39 -0700

    Create base image without "unattended-upgrades" package. * Fix user-data.yml. * Refactor wait-for-system-updates.sh

--- wait-for-system-updates.sh
+++ wait-for-system-updates.sh
@@ -1,53 +1,58 @@
 #!/usr/bin/env bash
 #
-# Wait up to 'timeout' seconds for a file to be free, i.e. no longer being accessed by any process on the system.
-# Additionally, ensure that the file is free for at least 'desired_time_for_file_to_be_free' consecutive seconds.
+# Wait up to TIMEOUT seconds for the script arguments (which are files) to be free, i.e. no longer accessed by any
+# process on the system. Additionally, ensure that they are free for at least DESIRED_TIME_FOR_FILES_TO_BE_FREE
+# consecutive seconds.
 #
-# The script is intended to be used with lock files, specifically /var/lib/dpkg/lock, which we need exclusive access
-# to when trying to update the Apt cache.
+# The script is intended to be used with lock files, specifically '/var/lib/dpkg/lock' and '/var/lib/apt/lists/lock',
+# which we need exclusive access to when trying to do anything with apt.
 
 # "set -e" isn't appropriate here because fuser is expected to return non-zero.
 
-elapsed=0
-timeout=360
-poll_interval=3
+ELAPSED=0
+TIMEOUT=360
+POLL_INTERVAL=3
 
-# Script will exit when the file has been free for this many consecutive seconds.
-desired_time_for_file_to_be_free=15
-# The number of consecutive seconds that the file has been free. Initialized to "-1", meaning "NOT FREE".
-time_file_has_been_free=-1
+# Script will exit when the files have been free for this many consecutive seconds.
+DESIRED_TIME_FOR_FILES_TO_BE_FREE=9
 
-lock_file=/var/lib/dpkg/lock
+# The number of consecutive seconds that the files have been free. Initialized to "-1", meaning "NOT FREE".
+TIME_FILES_HAVE_BEEN_FREE=-1
 
-# Loop until (elapsed >= timeout) or (time_file_has_been_free >= desired_time_for_file_to_be_free).
-while [ $elapsed -lt $timeout ]; do
-  sudo fuser $lock_file
+if [ $# -eq 0 ]; then
+    echo "ERROR: Specify the path(s) of file(s) to wait on."
+    exit 1
+fi
 
-  if [ $? -eq 0 ]; then    # File is being accessed by a process.
+# Loop until (ELAPSED >= TIMEOUT) or (TIME_FILES_HAVE_BEEN_FREE >= DESIRED_TIME_FOR_FILES_TO_BE_FREE).
+while [ $ELAPSED -lt $TIMEOUT ]; do
+  sudo fuser $*
+
+  if [ $? -eq 0 ]; then    # At least one file is being accessed by a process.
     # Set to "-1", meaning "NOT FREE".
-    time_file_has_been_free=-1
-  else    # File is free!
-    if [ $time_file_has_been_free -eq -1 ]; then
-      # File was previously "NOT FREE". It's free now, but it's only been that way 1 time (zero seconds).
-      time_file_has_been_free=0
+    TIME_FILES_HAVE_BEEN_FREE=-1
+  else    # All files are free!
+    if [ $TIME_FILES_HAVE_BEEN_FREE -eq -1 ]; then
+      # Files were previously "NOT FREE". They're free now, but it's only been so this instant (zero seconds).
+      TIME_FILES_HAVE_BEEN_FREE=0
     else
-      # File was also free "poll_interval" seconds ago, so add that time.
-      (( time_file_has_been_free += poll_interval ))
+      # Files were also free POLL_INTERVAL seconds ago, so add that time.
+      (( TIME_FILES_HAVE_BEEN_FREE += POLL_INTERVAL ))
     fi
   fi
 
-  if [ $time_file_has_been_free -ge $desired_time_for_file_to_be_free ]; then
-    break    # File has been free for at least 'desired_time_for_file_to_be_free' consecutive seconds.
+  if [ $TIME_FILES_HAVE_BEEN_FREE -ge $DESIRED_TIME_FOR_FILES_TO_BE_FREE ]; then
+    break  # Files have been free for at least DESIRED_TIME_FOR_FILES_TO_BE_FREE consecutive seconds.
   fi
 
-  sleep $poll_interval
-  (( elapsed += poll_interval ))
+  sleep $POLL_INTERVAL
+  (( ELAPSED += POLL_INTERVAL ))
 done
 
-if [ $elapsed -ge $timeout ]; then
-  >&2 echo "Exceeded timeout ($timeout seconds) waiting for the file lock."
+if [ $ELAPSED -ge $TIMEOUT ]; then
+  >&2 echo "Exceeded TIMEOUT ($TIMEOUT seconds) waiting for [$*] to become available."
   exit 1
 else
-  echo "File has been free for $desired_time_for_file_to_be_free consecutive seconds. Waited $elapsed seconds total."
+  echo "[$*] have been free for $DESIRED_TIME_FOR_FILES_TO_BE_FREE consecutive seconds. Waited $ELAPSED seconds total."
   exit 0
 fi
